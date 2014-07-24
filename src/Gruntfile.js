355b9aa1013054aabb70ca9a905a1d26e10332b4
Andrea Jemmett <andreajemmett@gmail.com>
2014-07-24 16:47:03 +0200

    Git stat graphs by hopping rabbit :)

--- src/Gruntfile.js
+++ src/Gruntfile.js
@@ -32,6 +32,8 @@ module.exports = function (grunt) {
 				src: [
 					'bower_components/jquery/dist/jquery.js',
 					'bower_components/bootstrap/dist/js/bootstrap.js',
+					'bower_components/d3/d3.js',
+					'bower_components/nvd3/nv.d3.js',
 					'contents/exclude/js/main.js'
 				],
 				dest: 'contents/main.js'
